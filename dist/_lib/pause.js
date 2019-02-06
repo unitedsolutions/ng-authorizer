bddd1f542fff34eed7efdecad59ee5516bafefe4
daretobebetter <30729632+daretobebetter@users.noreply.github.com>
2019-02-06 14:04:23 -0500

    Add files via upload

--- dist/_lib/pause.js
+++ dist/_lib/pause.js
@@ -1,2 +1,2 @@
-export default function (ms) { return new Promise(function (resolve) { return setTimeout(resolve, ms || 0); }); };
+export default (function (ms) { return new Promise(function (resolve) { return setTimeout(resolve, ms || 0); }); });
 //# sourceMappingURL=pause.js.map
